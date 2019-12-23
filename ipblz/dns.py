5c8fdee52da40c7a1420dd03bc3ec2dc6d470500
Eniy <44121977+Enigamict@users.noreply.github.com>
2019-12-23 23:25:36 +0900

    Delete dns.py

--- ipblz/dns.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-class dnshost:
-    def __init__(self, ip):
-        self.ip = ip
-
-    def dnsipback(self):
-        ipconversion = socket.gethostbyname(self.ip)
-        print(ipconversion)

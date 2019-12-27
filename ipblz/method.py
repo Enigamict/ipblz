a33827627f032854c4a705a72df65332235c3b46
Eniy <44121977+Enigamict@users.noreply.github.com>
2019-12-27 19:33:19 +0900

    Update method.py

--- ipblz/method.py
+++ ipblz/method.py
@@ -1,5 +1,5 @@
 import socket
-
+import time
 class Dns: # DNS（これいる？）
     def __init__(self, hostname):
         self.hostname = hostname
@@ -22,13 +22,25 @@ class Portscan: # ポートスキャナー TCP UDP TCP/SYN スキャンを実装
             print("{}/tcp Close".format(self.port))
 
 class Ping: # PINGは男の嗜み
-    def __init__(self, ip):
+    def __init__(self, ip, numbertimes):
         self.ip = ip
+        self.numbertimes = numbertimes
     def ping(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) # rawソケットに変えているのでpingを実行する場合管理者権限が必要になる
-        sock.sendto(b'\x08\x00\xf7\xff\x00\x00\x00\x00', (self.ip, 0)) # 迫真チェックサム計算部を忘れずに
-
-        data = sock.recv(255)
-
-        print(data)
-           
+        request = 0
+        reply = 0
+        try:
+            while True:
+                request += 1
+                sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) # rawソケットに変えているのでpingを実行する場合管理者権限が必要になる
+                time.sleep(1)
+                sock.sendto(b'\x08\x00\xf7\xff\x00\x00\x00\x00',(self.ip, 0)) # チェックサム計算を忘れずに
+                echoreply = sock.recv(255)
+                if echoreply[20] == 0: # 返ってきたEchoReplyのTypeを見ている
+                    reply += 1
+                    print("{} done".format(self.ip))
+                if request == self.numbertimes: # 指定した回数とrequestが同じになればbreak
+                    print("送信した回数 = {} 受信した回数 = {}".format(request, reply))
+                    break
+        except KeyboardInterrupt: # Ctrl-Cでキャンセル
+            print("Ctrl-C")
+            print("送信した回数 = {} 受信した回数 = {}".format(request, reply))

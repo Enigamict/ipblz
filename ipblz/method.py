5cc759c8a2abc0132b8e9957908dc85571ed39a0
Eniy <44121977+Enigamict@users.noreply.github.com>
2020-01-02 00:23:41 +0900

    Update method.py

--- ipblz/method.py
+++ ipblz/method.py
@@ -34,6 +34,7 @@ class Ping: # PINGは男の嗜み
                 sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) # rawソケットに変えているのでpingを実行する場合管理者権限が必要になる
                 time.sleep(1)
                 sock.sendto(b'\x08\x00\xf7\xff\x00\x00\x00\x00',(self.ip, 0)) # チェックサム計算を忘れずに
+                sock.settimeout(60)
                 echoreply = sock.recv(255)
                 if echoreply[20] == 0: # 返ってきたEchoReplyのTypeを見ている
                     reply += 1
@@ -44,6 +45,8 @@ class Ping: # PINGは男の嗜み
         except KeyboardInterrupt: # Ctrl-Cでキャンセル
             print("Ctrl-C")
             print("送信した回数 = {} 受信した回数 = {}".format(request, reply))
+        except socket.timeout: # タイムアウト処理 60秒経過でタイムアウトとなる
+            print("エラーです。タイムアウト")
 # -- 色々やってる -- #
 #class Arp:
 #    def __init__(self, ip):

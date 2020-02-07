3473c248684ab21ba90c28d7883559df7996f361
Eniy <44121977+Enigamict@users.noreply.github.com>
2020-02-08 01:56:54 +0900

    Add files via upload

---
+++ ipblz/header.py
@@ -0,0 +1,43 @@
+import struct
+from ctypes import *
+import socket
+
+class IP(Structure):
+    _fields_ = [
+        ("ihl",           c_uint8, 4),
+        ("version",       c_uint8, 4),
+        ("tos",           c_uint8),
+        ("len",           c_uint16),
+        ("id",            c_uint16),
+        ("offset",        c_uint16),
+        ("ttl",           c_uint8),
+        ("protocol_num",  c_uint8),
+        ("sum",           c_uint16),
+        ("src",           c_uint32),
+        ("dst",           c_uint32)
+    ]
+
+    def __new__(self, socket_buffer=None):
+        # ネットワークで受信したものを受け取り構造体にしている
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer=None):
+        # この処理でIPアドレスを見やすくしている
+        self.src_address = socket.inet_ntoa(struct.pack("<L",self.src))
+        self.dst_address = socket.inet_ntoa(struct.pack("<L",self.dst))
+
+class ICMP(Structure):
+
+    _fields_ = [
+        ("type",         c_uint8),
+        ("code",         c_uint8),
+        ("checksum",     c_uint16),
+        ("unused",       c_uint16),
+        ("next_hop_mtu", c_uint16)
+        ]
+
+    def __new__(self, socket_buffer):
+        return self.from_buffer_copy(socket_buffer)
+
+    def __init__(self, socket_buffer):
+        pass

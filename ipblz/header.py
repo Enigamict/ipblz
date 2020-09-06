70e0fa05c75597b119e74e75d53e27041baa5725
Enigamict <44121977+Enigamict@users.noreply.github.com>
2020-09-07 05:06:27 +0900

    Update header.py

--- ipblz/header.py
+++ ipblz/header.py
@@ -58,3 +58,20 @@ class ICMP(Structure):
 
     def __init__(self, socket_buffer):
         pass
+
+class pcap_hdr_s(Structure):
+    _fields_ = [
+        ("magic_number",         c_uint32),     # /* magic number */
+        ("version_major",        c_uint16),     # /* major version number */
+        ("version_minor",        c_uint16),     # /* minor version number */
+        ("thiszone",             c_int32),      # /* GMT to local correction */
+        ("sigfigs",              c_uint32),     # /* accuracy of timestamps */
+        ("snaplen",              c_uint32),     # /* max length of captured packets, in octets */
+        ("network",              c_uint32)      # /* data link type*/
+    ]
+
+    def __new__(self, pcap_buffer):
+        return self.from_buffer_copy(pcap_buffer)
+
+    def __init__(self, pcap_buffer):
+        pass
